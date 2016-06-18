a97cc685446bf110ea73e2aa4450d8b608eb4db2
Joe Winter <joe@joeventures.com>
2016-06-18 10:01:35 -0400

    Remove index.php from urls

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 
-	<title>The HTML5 Herald</title>
+	<title>52-Week Money Challenge<?php if(isset($title)) echo " | $title"; ?></title>
 	<meta name="author" content="Joe Winter">
 
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>">
