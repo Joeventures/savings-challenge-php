b48967031ef57237eeb4bfe55af4e590497c30eb
Joe Winter <joe@joeventures.com>
2016-06-19 17:40:37 -0400

    Correction of GitHub link in footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
 	<div class="container">
-		<p class="text-muted">Made by Joe Winter | <a href="https://github.com/Joeventures/savings-challenge">View on GitHub</a> | <a href="https://www.linkedin.com/in/joeventures/">Hire Me</a> | <a href="https://c4atlanta.org/support/">Support C4 Atlanta</a></p>
+		<p class="text-muted">Made by Joe Winter | <a href="https://github.com/Joeventures/savings-challenge-php">View on GitHub</a> | <a href="https://www.linkedin.com/in/joeventures/">Hire Me</a> | <a href="https://c4atlanta.org/support/">Support C4 Atlanta</a></p>
 	</div>
 </footer>
 <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
